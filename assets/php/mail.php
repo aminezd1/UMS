d19f29169126ee61e70df2d33d2b3171d45cebb5
aminezd1 <91153881+aminezd1@users.noreply.github.com>
2022-05-18 16:40:15 +0100

    Add files via upload

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,1 +1,14 @@
-
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$to = "amineziadi484@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers = "From: UMS.ma";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
